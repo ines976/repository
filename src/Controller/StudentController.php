bc195ddb6e6b81e7d52ad54671a5daa499679563
ines976 <ines.mabrouk@esprit.tn>
2023-02-05 23:45:41 +0100

    Initial commit

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,14 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController{
+#[Route('/index', name: 'index')]
+  public function index()
+ {
+    return new Response('Bonjour mes étudiants') ;
+ }
+}
+?>
