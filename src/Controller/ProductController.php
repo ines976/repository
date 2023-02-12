a5ff246cfe6905ae1ac6b2cd2513ef692a054d21
ines976 <ines.mabrouk@esprit.tn>
2023-02-13 00:45:39 +0100

    second commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+   // #[Route('/welcome', name: 'app_index')]
+    public function index2(): Response
+    {
+        return new Response ("Bonjour 3a17");
+    }
+}
